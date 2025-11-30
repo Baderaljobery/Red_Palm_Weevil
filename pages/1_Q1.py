8866b22a7738ab3a90e38971649a88ad13664b16
BADER ALJUBAYRI <421102053@s.mu.edu.sa>
2025-11-30 21:14:19 +0300

    Implement bar plot for 2024 inspections by region
    Added data visualization for inspections by region.

--- pages/1_Q1.py
+++ pages/1_Q1.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+url = "https://raw.githubusercontent.com/USERNAME/REPO/main/data/dataset.csv"
+df = pd.read_csv(url)
+
+st.title("السؤال 1: [اكتب السؤال]")
+st.write("شرح بسيط…")
+
+plt.figure(figsize=(10, 5))
+
+sns.barplot(data=df, x="Region", y="2024_inspection")
+
+plt.xticks(rotation=45, ha="right")
+plt.title("Number of inspections in 2024")
+plt.xlabel("Region")
+plt.ylabel("Number of inspections in 2024")
+plt.tight_layout()
+plt.show()
